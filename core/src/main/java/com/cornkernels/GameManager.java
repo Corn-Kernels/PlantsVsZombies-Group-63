2a052a60a46b383286a34f70d959cc870438092a
LovesSorrow <armanghadimi86@gmail.com>
2026-08-04 07:37:44 -0700

    Updated InputManager There are currently 3 problems this update solves: 1.The system can detect whether a key is pressed. 2.The system has the capability to rebind the actions associated with keys. 3.Missing presses between frames is solved using frameId;

--- core/src/main/java/com/cornkernels/GameManager.java
+++ core/src/main/java/com/cornkernels/GameManager.java
@@ -6,6 +6,7 @@ import com.cornkernels.engine.audio.AudioManager;
 import com.cornkernels.engine.input.InputManager;
 import com.cornkernels.engine.settings.AudioSettings;
 import com.cornkernels.engine.settings.GameSettings;
+import com.cornkernels.engine.settings.InputSettings;
 import com.cornkernels.engine.settings.VideoSettings;
 import com.cornkernels.engine.video.VideoManager;
 import com.cornkernels.game.screens.MenuScreen;
@@ -21,6 +22,7 @@ public class GameManager extends Game {
     private GameSettings gameSettings;
     private VideoSettings videoSettings;
     private AudioSettings audioSettings;
+    private InputSettings inputSettings;
 
     @Override
     public void create() {
@@ -29,20 +31,21 @@ public class GameManager extends Game {
         gameSettings = new GameSettings();
         videoSettings = new VideoSettings(gameSettings);
         audioSettings = new AudioSettings(gameSettings);
+        inputSettings = new InputSettings(gameSettings);
 
         audioManager = AudioManager.getInstance();
         audioManager.init(audioSettings);
         videoManager = VideoManager.getInstance();
         videoManager.init(videoSettings);
         inputManager = InputManager.getInstance();
-        inputManager.init(gameSettings);
+        inputManager.init(inputSettings);
 
         setScreen(new MenuScreen(this));
     }
 
     @Override
     public void render() {
-
+        inputManager.update();
     }
 
     @Override
